e61e49ded8457ddfe470659118c38ead2492b3a6
Bastien Michel <bamichel@ptb-02009329.paris.inria.fr>
2024-12-12 10:16:06 +0100

    update

--- SKINNY/MILP_trunc_diff_MITM.py
+++ SKINNY/MILP_trunc_diff_MITM.py
@@ -3,7 +3,6 @@
 import gurobipy as gp
 from gurobipy import GRB
 import numpy as np
-from tqdm import tqdm
 
 def tweakey(key):
     """Skinny tweaky.
@@ -49,6 +48,7 @@ def attack(structure_round, MITM_up_round, differential_round, MITM_down_round, 
         model.Params.MIRcuts = 2 #o for no cuts, 1 for classic cuts, 2 for agressive cuts
         #model.Params.Presolve = 2 
         model.Params.MIPFocus = 3 #1 for focusing feasible solution, 2 for optimality, 3 to focus on the bous (if best bound is not mooving)
+        model.Params.TimeLimit = 10000
 
         #ATTACK PARAMETERS
         total_round = structure_round + MITM_up_round + MITM_down_round + differential_round

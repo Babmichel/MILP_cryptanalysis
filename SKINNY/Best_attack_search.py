f58fb63a50d0e999e0d13dc82b2f768320d81549
Bastien Michel <bamichel@ptb-02009329.paris.inria.fr>
2024-12-12 10:03:14 +0100

    update

--- SKINNY/Best_attack_search.py
+++ SKINNY/Best_attack_search.py
@@ -1,23 +1,22 @@
 #search for the optimal attack with various parameters
 import numpy as np
-import sys
 import MILP_trunc_diff_MITM
 import Differential_MITM_MILP
-import parameters
+import parameters as p
 
-z = parameters["word_size"]
-Valid_matrix = np.zeros([parameters["structure_max"]+1,parameters["MITM_up_max"]+1,parameters["disitnguisher_max"]+1,parameters["MITM_down_max"]+1])
-Complexite_matrix = np.zeros([parameters["structure_max"]+1,parameters["MITM_up_max"]+1,parameters["disitnguisher_max"]+1,parameters["MITM_down_max"]+1])
-for structure_round in range(parameters["structure_min"],parameters["structure_max"]+1):
-    for MITM_up_round in range(parameters["MITM_up_min"],parameters["MITM_up_max"]+1):
-        for diff_round in range(parameters["disitnguisher_min"],parameters["disitnguisher_max"]+1):
-            for MITM_down_round in range(parameters["MITM_down_min"],parameters["MITM_down_max"]+1):
-                if (structure_round+MITM_up_round+diff_round+MITM_down_round >= parameters["attack_size_min"]) and (structure_round+MITM_up_round+diff_round+MITM_down_round <= parameters["attack_size_max"]):
+z = p.parameters["word_size"]
+Valid_matrix = np.zeros([p.parameters["structure_max"]+1,p.parameters["MITM_up_max"]+1,p.parameters["disitnguisher_max"]+1,p.parameters["MITM_down_max"]+1])
+Complexite_matrix = np.zeros([p.parameters["structure_max"]+1,p.parameters["MITM_up_max"]+1,p.parameters["disitnguisher_max"]+1,p.parameters["MITM_down_max"]+1])
+for structure_round in range(p.parameters["structure_min"],p.parameters["structure_max"]+1):
+    for MITM_up_round in range(p.parameters["MITM_up_min"],p.parameters["MITM_up_max"]+1):
+        for diff_round in range(p.parameters["distinguisher_min"],p.parameters["disitnguisher_max"]+1):
+            for MITM_down_round in range(p.parameters["MITM_down_min"],p.parameters["MITM_down_max"]+1):
+                if (structure_round+MITM_up_round+diff_round+MITM_down_round >= p.parameters["attack_size_min"]) and (structure_round+MITM_up_round+diff_round+MITM_down_round <= p.parameters["attack_size_max"]):
                     print("###########################################")
                     print("tentative :", structure_round, MITM_up_round, diff_round, MITM_down_round )
-                    if parameters["type_of_attack"] == 0:
+                    if p.parameters["type_of_attack"] == 0:
                         attaque = MILP_trunc_diff_MITM.attack(structure_round, MITM_up_round, diff_round+1, MITM_down_round-1,3)
-                    elif parameters["type_of_attack"] == 1:
+                    elif p.parameters["type_of_attack"] == 1:
                         attaque = Differential_MITM_MILP.attack(structure_round, MITM_up_round, diff_round, MITM_down_round,3)
                     if attaque[0]:
                         complexite_bleu = attaque[21]
@@ -25,12 +24,11 @@ for structure_round in range(parameters["structure_min"],parameters["structure_m
                         complexite_MATCH = attaque[23]
                         Valid_matrix[structure_round, MITM_up_round, diff_round, MITM_down_round] = 1
                         Complexite_matrix[structure_round, MITM_up_round, diff_round, MITM_down_round] = z*np.max([complexite_bleu, complexite_rouge, complexite_MATCH])
-for structure_round in range(parameters["structure_max"]+1):
-    for MITM_up_round in range(parameters["MITM_up_max"]+1):
-        for diff_round in range(parameters["disitnguisher_max"]+1):
-            for MITM_down_round in range(parameters["MITM_down_max"]+1):
-                if Valid_matrix[structure_round, MITM_up_round, diff_round, MITM_down_round] == 1 and Complexite_matrix[structure_round, MITM_up_round, diff_round, MITM_down_round] <= parameters["complexity_max"] :
+for structure_round in range(p.parameters["structure_max"]+1):
+    for MITM_up_round in range(p.parameters["MITM_up_max"]+1):
+        for diff_round in range(p.parameters["disitnguisher_max"]+1):
+            for MITM_down_round in range(p.parameters["MITM_down_max"]+1):
+                if Valid_matrix[structure_round, MITM_up_round, diff_round, MITM_down_round] == 1 and Complexite_matrix[structure_round, MITM_up_round, diff_round, MITM_down_round] <= p.parameters["complexity_max"] :
                     print("-------------------------------------------")
                     print("valid attack on ",structure_round + MITM_up_round+ diff_round+ MITM_down_round, " round, with complexity ", Complexite_matrix[structure_round, MITM_up_round, diff_round, MITM_down_round] )
                     print("parameters: ", structure_round, MITM_up_round, diff_round, MITM_down_round)
-
--- SKINNY/parameters.py
+++ SKINNY/parameters.py
@@ -2,7 +2,7 @@
 
 #Best attack search
 
-parameters = {"type_of_attack" : 'trunc', 
+parameters = {"type_of_attack" : 0, #0 for trunc , 1 for not trunc
 "attack_size_min" : 24,
 "attack_size_max" : 24,
 "complexity_max" : 204,
@@ -10,9 +10,9 @@ parameters = {"type_of_attack" : 'trunc',
 "structure_min" : 2,
 "structure_max" : 5,
 "MITM_up_min" : 2,
-"MITM_up_max" : 5,
-"distinguisher_min" : 2,
-"disitnguisher_max" : 5,
+"MITM_up_max" : 9,
+"distinguisher_min" : 3,
+"disitnguisher_max" : 10,
 "MITM_down_min" : 2,
-"MITM_down_max" : 5
+"MITM_down_max" : 8
 }

02c1b27e90d4cb1c8f9251b76765384e775fe5ce
Bastien Michel <bamichel@ptb-02009329.paris.inria.fr>
2024-12-19 10:41:30 +0100

    modif param

--- SKINNY/parameters.py
+++ SKINNY/parameters.py
@@ -5,7 +5,7 @@
 parameters = {"type_of_attack" : 0, #0 for trunc , 1 for not trunc
 "attack_size_min" : 24,
 "attack_size_max" : 24,
-"complexity_max" : 212,
+"complexity_max" : 200,
 "word_size" : 4,
 "structure_min" : 6,
 "structure_max" : 6,

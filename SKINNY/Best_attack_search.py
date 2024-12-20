a01b9be03c91b53f54ca456d039c1e086f437e64
Bastien Michel <bamichel@ptb-02009329.paris.inria.fr>
2024-12-20 11:01:53 +0100

    update parralelization

--- SKINNY/Best_attack_search.py
+++ SKINNY/Best_attack_search.py
@@ -22,26 +22,31 @@ Complexite_matrix = np.zeros([structure_max+1,MITM_up_max+1,disitnguisher_max+1,
 
 
 def search_attack(MITM_up_max2):
-    for structure_round in range(structure_max,structure_max+1):
+    for structure_round in range(structure_min,structure_max+1):
             for diff_round in range(distinguisher_min,disitnguisher_max+1):
                 for MITM_down_round in range(MITM_down_min,MITM_down_max+1):
                     if (structure_round+MITM_up_max2+diff_round+MITM_down_round >= p.parameters["attack_size_min"]) and (structure_round+MITM_up_max2+diff_round+MITM_down_round <= p.parameters["attack_size_max"]):
                         print("###########################################")
                         print("tentative :", structure_round, MITM_up_max2, diff_round, MITM_down_round )
                         if p.parameters["type_of_attack"] == 0:
-                            attaque = MILP_trunc_diff_MITM.attack(structure_round, MITM_up_max2, diff_round, MITM_down_round,3)
+                            attaque = MILP_trunc_diff_MITM.attack(structure_round, MITM_up_max2, diff_round+1, MITM_down_round-1,3)
                         elif p.parameters["type_of_attack"] == 1:
                             attaque = Differential_MITM_MILP.attack(structure_round, MITM_up_max2, diff_round, MITM_down_round,3)
-                        attaque.append([structure_round, MITM_up_max2, diff_round, MITM_down_round])
+                        attaque.append(structure_round)
+                        attaque.append(MITM_up_max2)
+                        attaque.append(diff_round)
+                        attaque.append(MITM_down_round)
+                        return attaque
                         
 with Pool(multiprocessing.cpu_count()) as pool:
-        attaque = (pool.map(search_attack, range(MITM_up_min, MITM_up_max+1)))                        
-        if attaque[0]:
-            complexite_bleu = attaque[21]
-            complexite_rouge = attaque[22]
-            complexite_MATCH = attaque[23]
-            Valid_matrix[attaque[24], attaque[25], attaque[26], attaque[28]] = 1
-            Complexite_matrix[attaque[24], attaque[25], attaque[26], attaque[28]] = z*np.max([complexite_bleu, complexite_rouge, complexite_MATCH])
+        attaque = (pool.map(search_attack, range(MITM_up_min, MITM_up_max+1)))    
+        print(attaque)                  
+        if attaque[-1][0]:
+            complexite_bleu = attaque[-1][21]
+            complexite_rouge = attaque[-1][22]
+            complexite_MATCH = attaque[-1][23]
+            Valid_matrix[attaque[-1][24], attaque[-1][25], attaque[-1][26], attaque[-1][27]] = 1
+            Complexite_matrix[attaque[-1][24], attaque[-1][25], attaque[-1][26], attaque[-1][27]] = z*np.max([complexite_bleu, complexite_rouge, complexite_MATCH])
 
 
 

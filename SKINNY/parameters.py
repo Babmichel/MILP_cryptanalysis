bd17e97e166d860015e06c9f5ca646dd7acc0986
Bastien Michel <bamichel@ptb-02009329.paris.inria.fr>
2025-01-06 09:25:34 +0100

    test parameters

--- SKINNY/parameters.py
+++ SKINNY/parameters.py
@@ -9,10 +9,10 @@ parameters = {"type_of_attack" : 0, #0 for trunc , 1 for not trunc
 "word_size" : 4,
 "structure_min" : 2,
 "structure_max" : 2,
-"MITM_up_min" : 2,
-"MITM_up_max" : 9,
-"distinguisher_min" : 1,
+"MITM_up_min" : 5,
+"MITM_up_max" : 8,
+"distinguisher_min" : 7,
 "disitnguisher_max" : 9,
-"MITM_down_min" : 2,
-"MITM_down_max" : 9
+"MITM_down_min" : 5,
+"MITM_down_max" : 8,
 }

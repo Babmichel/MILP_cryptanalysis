0527b0caaedcb5f52ab6eef4643e7e5a360427f5
Bastien Michel <bamichel@ptb-02009329.paris.inria.fr>
2025-01-07 13:56:11 +0100

    update param

--- SKINNY/parameters.py
+++ SKINNY/parameters.py
@@ -10,9 +10,9 @@ parameters = {"type_of_attack" : 0, #0 for trunc , 1 for not trunc
 "structure_min" : 2,
 "structure_max" : 2,
 "MITM_up_min" : 6,
-"MITM_up_max" : 6,
+"MITM_up_max" : 7,
 "distinguisher_min" : 8,
 "distinguisher_max" : 10,
-"MITM_down_min" : 7,
+"MITM_down_min" : 6,
 "MITM_down_max" : 7,
 }

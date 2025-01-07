39a099a49b99355d37bf2a26aa8905cf0b762514
Bastien Michel <bamichel@ptb-02009329.paris.inria.fr>
2025-01-07 13:31:05 +0100

    debuging

--- SKINNY/Best_attack_search.py
+++ SKINNY/Best_attack_search.py
@@ -14,18 +14,16 @@ MITM_down_max = p.parameters["MITM_down_max"]
 MITM_up_min = p.parameters["MITM_up_min"]
 distinguisher_min = p.parameters["distinguisher_min"]
 MITM_down_min = p.parameters["MITM_down_min"]
-
-
 z = p.parameters["word_size"]
-Valid_matrix = np.zeros([structure_max+1,MITM_up_max+1,distinguisher_max+1,MITM_down_max+1])
-Complexite_matrix = np.zeros([structure_max+1,MITM_up_max+1,distinguisher_max+1,MITM_down_max+1])
 
+Valid_matrix = np.zeros([structure_max+1, MITM_up_max+1, distinguisher_max+1, MITM_down_max+1])
+Complexite_matrix = np.zeros([structure_max+1, MITM_up_max+1, distinguisher_max+1, MITM_down_max+1])
 
 def search_attack(MITM_up_max2):
-    for structure_round in range(structure_min,structure_max+1):
-            for diff_round in range(distinguisher_min, distinguisher_max+1):
-                for MITM_down_round in range(MITM_down_min,MITM_down_max+1):
-                    if (structure_round+MITM_up_max2+diff_round+MITM_down_round >= p.parameters["attack_size_min"]) and (structure_round+MITM_up_max2+diff_round+MITM_down_round <= p.parameters["attack_size_max"]):
+    for structure_round in range(structure_min, structure_max + 1):
+            for diff_round in range(distinguisher_min, distinguisher_max + 1):
+                for MITM_down_round in range(MITM_down_min, MITM_down_max + 1):
+                    if (structure_round + MITM_up_max2 + diff_round + MITM_down_round >= p.parameters["attack_size_min"]) and (structure_round+MITM_up_max2+diff_round+MITM_down_round <= p.parameters["attack_size_max"]):
                         print("###########################################")
                         print("tentative :", structure_round, MITM_up_max2, diff_round, MITM_down_round )
                         if p.parameters["type_of_attack"] == 0:
@@ -39,23 +37,21 @@ def search_attack(MITM_up_max2):
                         return attaque
                         
 with Pool(multiprocessing.cpu_count()) as pool:
-        attaque = (pool.map(search_attack, range(MITM_up_min, MITM_up_max+1)))                     
-        if attaque[-1][0]:
-            complexite_bleu = attaque[-1][21]
-            complexite_rouge = attaque[-1][22]
-            complexite_MATCH = attaque[-1][23]
-            Valid_matrix[attaque[-1][24], attaque[-1][25], attaque[-1][26], attaque[-1][27]] = 1
-            Complexite_matrix[attaque[-1][24], attaque[-1][25], attaque[-1][26], attaque[-1][27]] = z*np.max([complexite_bleu, complexite_rouge, complexite_MATCH])
+    attaque = (pool.map(search_attack, range(MITM_up_min, MITM_up_max+1)))                     
+    if attaque[-1][0]:
+        complexite_bleu = attaque[-1][21]
+        complexite_rouge = attaque[-1][22]
+        complexite_MATCH = attaque[-1][23]
+        Valid_matrix[attaque[-1][24], attaque[-1][25], attaque[-1][26], attaque[-1][27]] = 1
+        Complexite_matrix[attaque[-1][24], attaque[-1][25], attaque[-1][26], attaque[-1][27]] = z*np.max([complexite_bleu, complexite_rouge, complexite_MATCH])
 
-
-
-for structure_round in range(p.parameters["structure_max"]+1):
-    for MITM_up_round in range(p.parameters["MITM_up_max"]+1):
-        for diff_round in range(p.parameters["distinguisher_max"]+1):
-            for MITM_down_round in range(p.parameters["MITM_down_max"]+1):
+for structure_round in range(structure_max+1):
+    for MITM_up_round in range(MITM_up_max+1):
+        for diff_round in range(distinguisher_max+1):
+            for MITM_down_round in range(MITM_down_max+1):
                 if Valid_matrix[structure_round, MITM_up_round, diff_round, MITM_down_round] == 1 and Complexite_matrix[structure_round, MITM_up_round, diff_round, MITM_down_round] <= p.parameters["complexity_max"] :
                     print("-------------------------------------------")
-                    print("valid attack on ",structure_round + MITM_up_round+ diff_round+ MITM_down_round, " round, with complexity ", Complexite_matrix[structure_round, MITM_up_round, diff_round, MITM_down_round] )
+                    print("valid attack on ",structure_round + MITM_up_round + diff_round + MITM_down_round, " round, with complexity ", Complexite_matrix[structure_round, MITM_up_round, diff_round, MITM_down_round])
                     print("parameters: ", structure_round, MITM_up_round, diff_round, MITM_down_round)
 
 print("FIN RECHERCHE")
--- SKINNY/parameters.py
+++ SKINNY/parameters.py
@@ -5,14 +5,14 @@
 parameters = {"type_of_attack" : 0, #0 for trunc , 1 for not trunc
 "attack_size_min" : 24,
 "attack_size_max" : 24,
-"complexity_max" : 200,
+"complexity_max" : 201,
 "word_size" : 4,
 "structure_min" : 2,
 "structure_max" : 2,
 "MITM_up_min" : 6,
-"MITM_up_max" : 7,
+"MITM_up_max" : 6,
 "distinguisher_min" : 8,
 "distinguisher_max" : 10,
-"MITM_down_min" : 6,
+"MITM_down_min" : 7,
 "MITM_down_max" : 7,
 }

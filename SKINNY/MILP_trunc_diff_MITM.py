6b2d16f201213f4b080af521d413628d33b034ce
Bastien Michel <bamichel@ptb-02009329.paris.inria.fr>
2025-01-14 09:22:09 +0100

    update match complexity with sieving the state test

--- SKINNY/MILP_trunc_diff_MITM.py
+++ SKINNY/MILP_trunc_diff_MITM.py
@@ -220,18 +220,20 @@ def attack(structure_round, MITM_up_round, differential_round, MITM_down_round, 
         
         blue_complexity = cost_differential + count_blue + MITM_down_guess + state_test_up
         red_complexity = cost_differential + count_red + MITM_up_guess + state_test_down + end_differential - start_differential
-        MATCH_complexity = cost_differential + count_blue + count_red - count_equation + state_test_down + state_test_up + end_differential + 16-2*fix_quantity_structure - 2*(16-fix_quantity_structure) + count_missing_equation
+        MATCH_complexity = cost_differential + count_blue + count_red - count_equation + state_test_down + state_test_up + end_differential + 16-2*fix_quantity_structure - 2*(16-fix_quantity_structure) + count_missing_equation - state_test_down - state_test_up
+        
         #MATCH_complexity = blue_complexity + red_complexity + (fix_quantity_structure - end_structure_active - start_structure_active)- 2*(16-fix_quantity_structure) + count_missing_equation - count_equation - ((cost_differential - start_differential) - (16-start_structure_active)) - MITM_down_guess - MITM_up_guess
-        model.addConstr(blue_complexity <= complexite_bleu)
-        model.addConstr(red_complexity <= complexite_rouge)
-        model.addConstr(MATCH_complexity <= complexite_match)
-        model.addConstr(MATCH_complexity <= 48)
-        model.addConstr(blue_complexity <= 47)
-        model.addConstr(red_complexity <= 47)
+        model.addConstr(blue_complexity <= complexity + complexite_bleu)
+        model.addConstr(red_complexity <= complexity + complexite_rouge)
+        model.addConstr(MATCH_complexity <= complexity + complexite_match)
+        #model.addConstr(MATCH_complexity <= 46)
+        #model.addConstr(blue_complexity <= 47)
+        #model.addConstr(red_complexity <= 47)
+        
         #Objective : Minimize the attack complexity
         model.setObjectiveN(complexity + complexite_rouge + complexite_bleu + complexite_match, 0, 10)
-        model.setObjectiveN(-1*gp.quicksum(binary_bound_for_key_knowledge[row, col, 0, color, 0] for row in range(4) for col in range(4) for color in range(2)), 1, 8)
-        model.setObjectiveN(-1*(state_test_down + state_test_up), 2, 6)
+        #model.setObjectiveN(-1*gp.quicksum(binary_bound_for_key_knowledge[row, col, 0, color, 0] for row in range(4) for col in range(4) for color in range(2)), 1, 8)
+        #model.setObjectiveN(-1*(state_test_down + state_test_up), 2, 6)
         
         model.ModelSense = GRB.MINIMIZE
 

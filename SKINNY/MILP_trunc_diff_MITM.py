b518667f24d6f98adaf227c2d8b087671d0cfd18
Bastien Michel <bamichel@ptb-02009329.paris.inria.fr>
2025-02-19 15:20:44 +0100

    remove match since it's not the bottleneck

--- SKINNY/MILP_trunc_diff_MITM.py
+++ SKINNY/MILP_trunc_diff_MITM.py
@@ -1,5 +1,5 @@
 #Differential_MITM_MILP
-pip install gurobipy
+
 import gurobipy as gp
 from gurobipy import GRB
 import numpy as np
@@ -233,7 +233,7 @@ def attack(structure_round, MITM_up_round, differential_round, MITM_down_round, 
   
         model.addConstr(blue_complexity <= complexity)
         model.addConstr(red_complexity <= complexity)
-        model.addConstr(MATCH_complexity <= complexity)
+        #model.addConstr(MATCH_complexity <= complexity)
 
         model.addConstr(complexity <= 48)
         #model.addConstr(blue_complexity <= complexity - complexite_bleu)

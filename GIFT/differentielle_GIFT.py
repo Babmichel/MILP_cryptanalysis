b863072f40d9e0cbc0d65f10a63371c2b57a4eb5
Bastien Michel <bamichel@ptb-02009329.paris.inria.fr>
2025-04-18 14:07:26 +0200

    fixing everything for test

--- GIFT/differentielle_GIFT.py
+++ GIFT/differentielle_GIFT.py
@@ -179,30 +179,30 @@ def diff_gift(round_number = 10, multi_search = False, number_of_solution = 1000
         #fixing some bits at the start of the distinguisher 
         
         #start = [i for i in range(64)]
-        start = [9, 10, 12, 28, 29, 40, 45, 46, 56, 57]
-        #start = [9, 10, 45, 46]
+        #start = [9, 10, 12, 28, 29, 40, 45, 46, 56, 57]
+        start = [9, 10, 45, 46]
         for bit in range(64) :
             model.addConstr(state[0, 0, bit] == 0)
             model.addConstr(state[0, 1, bit] == 0)
             if bit not in start :
                 model.addConstr(state[0, 2, bit] == 0, name = f'fix the difference to 0 in bit_{bit} in the start of distinguisher')
-        model.addConstr(gp.quicksum(state[0, 2, bit] for bit in range(64)) >= 1, name = f'at least one active difference at the start')
+        model.addConstr(gp.quicksum(state[0, 2, bit] for bit in range(64)) >= 4, name = f'at least one active difference at the start')
                 
         
         #fixing some bits at the end of the distinguisher 
         
         #end = [i for i in range(64)]
-        end = [0, 1, 2, 3, 4, 5, 6, 7, 32, 33, 34, 35, 36, 37, 38, 39]
-        #end = [3, 5, 33, 39]
+        #end = [0, 1, 2, 3, 4, 5, 6, 7, 32, 33, 34, 35, 36, 37, 38, 39]
+        end = [3, 5, 33, 39]
         #end =[]
         for bit in range(64) :
             if bit not in end:
                model.addConstr(state[round_number - 1, 2, bit] == 0, name = f'fix the difference to 0 in bit_{bit} in the end of distinguisher')
-        model.addConstr(gp.quicksum(state[round_number-1, 2, bit] for bit in range(64)) >= 1, name = f'at least one active difference at the end')
+        model.addConstr(gp.quicksum(state[round_number-1, 2, bit] for bit in range(64)) >= 4, name = f'at least one active difference at the end')
         
 
         #Fixing the differences of the key
-        """
+        
         for key_elem in [0, 1, 3, 4, 5, 6, 7]:
             for bit in range(16):
                 model.addConstr(key[key_elem, bit] == 0, name = f"fixing the key element to 0 : {key_elem}-{bit}")
@@ -212,8 +212,8 @@ def diff_gift(round_number = 10, multi_search = False, number_of_solution = 1000
                 model.addConstr(key[2, bit] == 1, name = f"fixing the key element to 1 : 3-{bit}")
             else :
               model.addConstr(key[2, bit] == 0, name = f"fixing the key element to 0 : 3-{bit}")
-        """
         
+
         #Counting information
         key_diff_count = gp.quicksum(key[k, index] for k in range(8) for index in range(16)) #quantity of differences in the key 
 
